23df16a9df640d49daecb3f7ea5a9cecec65f013
AyushBhardwaj-AdRuntime <ayushbhardwaj1334@gmail.com>
2024-07-15 19:44:26 +0000

    conversion

---
+++ 1basics/5datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+//alert ( 3 + 3 ) we are using nodejs , not browser
+
+console.log( 3 + 3);  // code readibility is must so try to put semicolon in the end wherever is required  
+console.log( " hitesh ")
+let name = "ayush"
+let age = 19
+let isLoggedIn = false
+let temprature = null
+
+
+// number => 2 to power 52
+// bigint
+//string =>""
+//boolean
+//null => stand alone value
+// undiefined =>
+    // symbol ==> unique
+
+
+// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined 
---
+++ 1basics/6ConversionOperation.js
@@ -0,0 +1,32 @@
+let score = ("33"); // we can convert the string into number by  using valueInNumber  
+let  scoreInNumber = Number (score)
+//console.log(scoreInNumber);
+//console.log(typeof scoreInNumber);
+
+let value = ("33abc")
+let valueInNumber = Number  (value)
+//console.log(valueInNumber); // but is shows NAN 
+//console.log(typeof valueInNumber); // it is number type 
+
+//notes
+//  "33 " => Number 33
+//   "33abc " => NaN NOT A NUMBER 
+//     TRUE => 1 and FALSE => 0 
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ // console.log(booleanIsLoggedIn);
+ // notes
+ // 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
