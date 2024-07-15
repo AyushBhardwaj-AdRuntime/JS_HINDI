466f03acf6d6c68a9271f0aed7ff60596db6fcdd
AyushBhardwaj-AdRuntime <ayushbhardwaj1334@gmail.com>
2024-07-15 09:14:12 +0000

    datatype

---
+++ 1basics/variable.js
@@ -0,0 +1,19 @@
+console.log("raj")
+const accountId = 1334
+let accountEmail = "ayushbhardwaj1334@gmail"
+var accountPassword 
+accountCity = "goa"
+
+
+
+accountPassword = "876543331"
+accountEmail = "ayush1334@gmail"
+accountCity = "ga"
+let accountState;
+ 
+
+//  prefer not to use var because of issue  in block scope //
+
+
+
+console.table([accountEmail , accountPassword , accountCity , accountState]);
